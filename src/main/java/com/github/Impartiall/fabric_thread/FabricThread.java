9d299ada8adf5ca5d2d0af7c6a183f7fb3043799
Semifake <42242696+Semifake@users.noreply.github.com>
2020-08-26 17:25:12 -0400

    Add boilerplate

--- src/main/java/com/github/Impartiall/fabric_thread/FabricThread.java
+++ src/main/java/com/github/Impartiall/fabric_thread/FabricThread.java
@@ -1,1 +1,10 @@
-package com.github.impartiall.fabric_thread
+package com.github.impartiall.fabric_thread;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CoolMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {}
+
+}
